32cabcf8f670b6575b1e7dfc22b3fdc32753756d
Cesar Ramirez <cesar.ramsan@gmail.com>
2013-11-13 02:52:48 -0600

    Added more sorting algorithms

--- Fun/Main.cs
+++ Fun/Main.cs
@@ -4,11 +4,12 @@ namespace Fun
 {
 	class MainClass
 	{
-		public const int LIST_SIZE = 3;
+		public const int LIST_SIZE = 10;
 
 		public static void Main (string[] args)
 		{
 			int[] randomList = new int[LIST_SIZE];
+			//fillWithUniqueRandomNumbers(randomList);
 			fillWithRandomNumbers(randomList);
 			for (int i =0; i < randomList.Length; i++) 
 			{
@@ -17,7 +18,10 @@ namespace Fun
 			Console.Out.WriteLine("Now sorted: ");
 			//bubbleSort(randomList);
 			//insertionSort(randomList);
-			mergeSort(randomList);
+			//mergeSort(randomList);
+			//quicksort(randomList);
+			//quicksortinplace(randomList, 0, randomList.Length);
+			selectionsort(randomList);
 			for (int i =0; i < randomList.Length; i++) 
 			{
 				Console.Out.WriteLine(randomList[i]);
@@ -30,7 +34,25 @@ namespace Fun
 			Random rnd = new Random();
 			for (int i =0; i < list.Length; i++) 
 			{
-				list[i] = rnd.Next(LIST_SIZE * 2);
+				list[i] = rnd.Next(LIST_SIZE);
+			}
+		}
+
+		public static void fillWithUniqueRandomNumbers (int[] list)
+		{
+			Random rnd = new Random();
+			int index;
+			for (int i = 1; i <= list.Length; i++) 
+			{
+				index = rnd.Next(LIST_SIZE);
+				if(list[index] == 0)
+				{
+					list[index] = i;
+				}
+				else
+				{
+					i--;
+				}
 			}
 		}
 
@@ -76,12 +98,8 @@ namespace Fun
 			}
 		}
 
+		//Complexity O(n log(n))
 		public static void mergeSort(int[] list)
-		{
-			splitThenMerge(list);
-		}
-
-		private static void splitThenMerge(int[] list)
 		{
 			split(list, 0, list.Length);
 		}
@@ -95,7 +113,7 @@ namespace Fun
 				{
 					isOdd = 1;
 				}
-				newLength = length/2 ;
+				newLength = (length/2);
 				split(list, start, newLength + isOdd);
 				split(list, start + newLength + isOdd , newLength);
 				merge(list, start, newLength + isOdd, start + newLength + isOdd , newLength);
@@ -104,22 +122,162 @@ namespace Fun
 
 		private static void merge(int[] list, int startA, int lengthA, int startB, int lenghtB)
 		{
-			int i = 0, j = 0, k = startA, temp;
+			int i = 0, j = 0, k = 0;
 
-			do {
-				if(list[startA + i] > list[startB + j])
+			int[] outlist = new int[lenghtB + lengthA];
+
+			for(int p = 0; p < outlist.Length; p++)
+			{
+				if(j >= lengthA)
 				{
-					temp = list[startA + i];
-					list[k] = list[startB + j];
-					list[startB + j] = temp;
+					outlist[k] = list[startB + i];
+					i++;
+				}
+				else if (i >= lenghtB)
+				{
+					outlist[k] = list[startA + j];
+					j++;
+				} 
+				else if(list[startA + j] > list[startB + i])
+				{
+					outlist[k] = list[startB + i];
 					i++;
 				}
 				else
 				{
+					outlist[k] = list[startA + j];
 					j++;
 				}
-			}while(i < lengthA && j < lenghtB );
+				k++;
+			}
 
+			for(int p = 0; p < outlist.Length; p++)
+			{
+				list[startA + p] = outlist[p];
+			}
+
+		}
+
+		public static void quicksort (int[] list)
+		{
+			partition(list, 0, list.Length);
+		}
+
+		private static void partition (int[] list, int start, int length)
+		{
+			if (length < 2) {
+				return;
+			}
+
+			Random rnd = new Random ();
+			int pivotIndex = start + rnd.Next (length);
+			int pivot = list [pivotIndex];
+			int left = start, right = start + length - 1;
+
+			while (left < right) {
+				while(list [left] < pivot) 
+				{
+					left++;
+				}
+				while(list[right] > pivot )
+				{
+					right--;
+				}
+				swap(list, right, left);
+			}
+			pivotIndex = left;
+
+			partition(list, start, pivotIndex - start );
+			partition(list, pivotIndex + 1, start + length - pivotIndex - 1);
+		}
+
+		//Not working yet
+		public static void quicksortinplace (int[] list, int start, int length)
+		{
+			if(length < 2)
+			{
+				return;
+			}
+			int pivotIndex = partitioninplace(list, start, length);
+			quicksortinplace(list, start, pivotIndex - start);
+			quicksortinplace(list, pivotIndex + 1,  length - pivotIndex - 1);
+		}
+		
+		private static int partitioninplace (int[] list, int start, int length)
+		{
+			Random rnd = new Random ();
+			int pivotIndex = start + rnd.Next (length);
+			pivotIndex = 2;
+			int pivot = list [pivotIndex];
+			swap(list, pivotIndex, length - 1);
+			int left = start, right = start + length - 1;
+			int storeIndex = left;
+			while (left < right) {
+				if(list[left] <= pivot)
+				{
+					swap(list, left, storeIndex);
+					storeIndex++;
+				}
+				left++;
+			}
+			swap(list, storeIndex, right);
+			return storeIndex;
+		}
+
+		public static int binarySearch(int[] list, int term)
+		{
+			int max = list.Length, min = 0;
+			int middle = -1;
+			while (max - min > 1 ) {
+				middle = (min + max) / 2;
+				if (term < list[middle]) {
+					max = middle;
+				} else {
+					min = middle;
+				}
+			}
+			if (list[min] == term) {
+				return min;
+			} else {
+				return -1;
+			}
+
+		}
+	
+		public static void selectionsort (int[] list)
+		{
+			int index;
+			int min;
+			for (int i = 0; i < list.Length - 1; i++) {
+				index = i;
+				min = Int16.MaxValue;
+				for (int j = i; j < list.Length; j++) {
+					if(list[j] < min)
+					{
+						min = list[j];
+						index = j;
+					}
+				}
+				swap(list, index, i);
+			}
+		}
+
+		private static void swap(int[] list, int a, int b)
+		{
+//			for (int i =0; i < list.Length; i++) 
+//			{
+//				Console.Out.Write(list[i] + "-");
+//			}
+//			Console.Out.WriteLine();
+			int temp = list[a];
+			list[a] = list[b];
+			list[b] = temp;
+//			for (int i =0; i < list.Length; i++) 
+//			{
+//				Console.Out.Write(list[i] + "-");
+//			}
+//			Console.Out.WriteLine();
+//			Console.Out.WriteLine();
 		}
 	}
 }
